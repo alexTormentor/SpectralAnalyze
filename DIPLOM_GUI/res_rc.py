8ecf465e1d4324861a6ec176577a67a30f17c3de
Alex <alexanderbor0134@mail.ru>
2023-06-10 20:38:57 +0300

    UI features

---
+++ DIPLOM_GUI/res_rc.py
@@ -0,0 +1,201 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.5.1
+# WARNING! All changes made in this file will be lost!
+
+from PySide6 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x01g\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 height=\x222\
+4px\x22 viewBox=\x220 \
+0 24 24\x22 width=\x22\
+24px\x22 fill=\x22#FFF\
+FFF\x22><path d=\x22M0\
+ 0h24v24H0V0z\x22 f\
+ill=\x22none\x22/><pat\
+h d=\x22M3 17.25V21\
+h3.75L17.81 9.94\
+l-3.75-3.75L3 17\
+.25zM5.92 19H5v-\
+.92l9.06-9.06.92\
+.92L5.92 19zM20.\
+71 5.63l-2.34-2.\
+34c-.2-.2-.45-.2\
+9-.71-.29s-.51.1\
+-.7.29l-1.83 1.8\
+3 3.75 3.75 1.83\
+-1.83c.39-.39.39\
+-1.02 0-1.41z\x22/>\
+</svg>\
+\x00\x00\x01\x0c\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 height=\x224\
+8\x22 viewBox=\x220 -9\
+60 960 960\x22 widt\
+h=\x2248\x22><path d=\x22\
+M772-367q-47 0-7\
+9-32.5T661-479q0\
+-47 32-79.5t79-3\
+2.5q46 0 78.5 32\
+.5T883-479q0 47-\
+32.5 79.5T772-36\
+7Zm-410 78-42-43\
+ 114-116H76v-62h\
+358L320-625l42-4\
+3 188 189-188 19\
+0Z\x22/></svg>\
+\x00\x00\x00\xd1\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 height=\x222\
+4px\x22 viewBox=\x220 \
+0 24 24\x22 width=\x22\
+24px\x22 fill=\x22#FFF\
+FFF\x22><path d=\x22M0\
+ 0h24v24H0V0z\x22 f\
+ill=\x22none\x22/><pat\
+h d=\x22M9 16.2L4.8\
+ 12l-1.4 1.4L9 1\
+9 21 7l-1.4-1.4L\
+9 16.2z\x22/></svg>\
+\
+\x00\x00\x03)\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 enable-ba\
+ckground=\x22new 0 \
+0 24 24\x22 height=\
+\x2224px\x22 viewBox=\x22\
+0 0 24 24\x22 width\
+=\x2224px\x22 fill=\x22#F\
+FFFFF\x22><g><rect \
+fill=\x22none\x22 heig\
+ht=\x2224\x22 width=\x222\
+4\x22 x=\x220\x22/></g><g\
+><g><polygon poi\
+nts=\x2220,7 20.94,\
+4.94 23,4 20.94,\
+3.06 20,1 19.06,\
+3.06 17,4 19.06,\
+4.94\x22/><polygon \
+points=\x228.5,7 9.\
+44,4.94 11.5,4 9\
+.44,3.06 8.5,1 7\
+.56,3.06 5.5,4 7\
+.56,4.94\x22/><poly\
+gon points=\x2220,1\
+2.5 19.06,14.56 \
+17,15.5 19.06,16\
+.44 20,18.5 20.9\
+4,16.44 23,15.5 \
+20.94,14.56\x22/><p\
+ath d=\x22M17.71,9.\
+12l-2.83-2.83C14\
+.68,6.1,14.43,6,\
+14.17,6c-0.26,0-\
+0.51,0.1-0.71,0.\
+29L2.29,17.46c-0\
+.39,0.39-0.39,1.\
+02,0,1.41 l2.83,\
+2.83C5.32,21.9,5\
+.57,22,5.83,22s0\
+.51-0.1,0.71-0.2\
+9l11.17-11.17C18\
+.1,10.15,18.1,9.\
+51,17.71,9.12z M\
+14.17,8.42l1.41,\
+1.41 L14.41,11L1\
+3,9.59L14.17,8.4\
+2z M5.83,19.59l-\
+1.41-1.41L11.59,\
+11L13,12.41L5.83\
+,19.59z\x22/></g></\
+g></svg>\
+\x00\x00\x00\xcf\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 height=\x222\
+4px\x22 viewBox=\x220 \
+0 24 24\x22 width=\x22\
+24px\x22 fill=\x22#FFF\
+FFF\x22><path d=\x22M0\
+ 0h24v24H0V0z\x22 f\
+ill=\x22none\x22/><pat\
+h d=\x22M10 8.64L15\
+.27 12 10 15.36V\
+8.64M8 5v14l11-7\
+L8 5z\x22/></svg>\
+"
+
+qt_resource_name = b"\
+\x00\x04\
+\x00\x06\xfa^\
+\x00i\
+\x00c\x00o\x00n\
+\x00\x05\
+\x00o\xa6S\
+\x00i\
+\x00c\x00o\x00n\x00s\
+\x00\x15\
+\x0a\x8f\xb1G\
+\x00c\
+\x00r\x00e\x00a\x00t\x00e\x00_\x00w\x00h\x00i\x00t\x00e\x00_\x002\x004\x00d\x00p\
+\x00.\x00s\x00v\x00g\
+\x00#\
+\x0d\xdaMG\
+\x00s\
+\x00t\x00e\x00p\x00_\x00F\x00I\x00L\x00L\x000\x00_\x00w\x00g\x00h\x00t\x004\x000\
+\x000\x00_\x00G\x00R\x00A\x00D\x000\x00_\x00o\x00p\x00s\x00z\x004\x008\x00.\x00s\
+\x00v\x00g\
+\x00\x13\
+\x094\xe6G\
+\x00d\
+\x00o\x00n\x00e\x00_\x00w\x00h\x00i\x00t\x00e\x00_\x002\x004\x00d\x00p\x00.\x00s\
+\x00v\x00g\
+\x00\x1c\
+\x05\xde\x1c\xa7\
+\x00a\
+\x00u\x00t\x00o\x00_\x00f\x00i\x00x\x00_\x00h\x00i\x00g\x00h\x00_\x00w\x00h\x00i\
+\x00t\x00e\x00_\x002\x004\x00d\x00p\x00.\x00s\x00v\x00g\
+\x00\x19\
+\x0b\xba\xce'\
+\x00p\
+\x00l\x00a\x00y\x00_\x00a\x00r\x00r\x00o\x00w\x00_\x00w\x00h\x00i\x00t\x00e\x00_\
+\x002\x004\x00d\x00p\x00.\x00s\x00v\x00g\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x0e\x00\x02\x00\x00\x00\x05\x00\x00\x00\x03\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\xc6\x00\x00\x00\x00\x00\x01\x00\x00\x03P\
+\x00\x00\x01\x88\x8c\xe8\xe8\x16\
+\x00\x00\x00\x9a\x00\x00\x00\x00\x00\x01\x00\x00\x02{\
+\x00\x00\x01\x88\x86\x9a<\xfd\
+\x00\x00\x00\x1e\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x88\x8c\xe7\xfb\xe2\
+\x00\x00\x01\x04\x00\x00\x00\x00\x00\x01\x00\x00\x06}\
+\x00\x00\x01\x88\x8c\xe9\xef\xa0\
+\x00\x00\x00N\x00\x00\x00\x00\x00\x01\x00\x00\x01k\
+\x00\x00\x01\x88\x8c\xd9\xcb\xb8\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
